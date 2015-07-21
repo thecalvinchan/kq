f31eac66ed24293ed33b639d0364616d9b00fe60
Calvin Chan <calvin.chan.h@gmail.com>
2015-07-21 11:36:08 -0400

    player controls work

--- js/app.js
+++ js/app.js
@@ -27,17 +27,47 @@ return Backbone.View.extend({
     create: function() {
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.game.add.sprite(0,0,'sky');
-        var platforms = this.game.add.group();
-        platforms.enableBody = true;
-        var ground = platforms.create(0, this.game.world.height - 64, 'ground');
+        this.platforms = this.game.add.group();
+        this.platforms.enableBody = true;
+        var ground = this.platforms.create(0, this.game.world.height - 64, 'ground');
         ground.scale.setTo(2,2);
         ground.body.immovable = true;
-        var ledge = platforms.create(400,400, 'ground');
+        var ledge = this.platforms.create(400,400, 'ground');
         ledge.body.immovable = true;
-        ledge = platforms.create(-150, 250, 'ground');
+        ledge = this.platforms.create(-150, 250, 'ground');
         ledge.body.immovable = true;
+
+        //player
+        this.player = this.game.add.sprite(32, this.game.world.height - 150, 'dude');
+        this.game.physics.arcade.enable(this.player);
+
+        this.player.body.bounce.y = 0.2;
+        this.player.body.gravity.y = 300;
+        this.player.body.collideWorldBounds = true;
+        
+        this.player.animations.add('left', [0,1,2,3], 10, true);
+        this.player.animations.add('right', [5,6,7,8], 10, true);
     },
     update: function() {
+        this.game.physics.arcade.collide(this.player, this.platforms);
+        
+        var cursors = this.game.input.keyboard.createCursorKeys();
+
+        this.player.body.velocity.x = 0;
+        if (cursors.left.isDown) {
+            this.player.body.velocity.x = -150;
+            this.player.animations.play('left');
+        } else if (cursors.right.isDown) {
+            this.player.body.velocity.x = 150;
+            this.player.animations.play('right');
+        } else {
+            this.player.animations.stop();
+            this.player.frame = 4;
+        }
+
+        if (cursors.up.isDown && this.player.body.touching.down) {
+            this.player.body.velocity.y = -350;
+        }
     }
 });
 });
